17e25dd89cc41f23fd44bc042f20484261204b16
Will Paul <wpaul@constantcontact.com>
2019-08-29 11:06:05 -0400

    Implement a bunch of list ops w/folds

--- src/main/scala/LinkedList.scala
+++ src/main/scala/LinkedList.scala
@@ -1,16 +1,43 @@
+import scala.annotation.tailrec
+
 sealed trait LinkedList[+A]
 case object Nil extends LinkedList[Nothing]
 case class Cons[+A](head: A, tail: LinkedList[A]) extends LinkedList[A]
 
 object LinkedList {
-  def sum[N](nums: LinkedList[N])(implicit numeric: Numeric[N]): N = nums match {
+  @tailrec
+  def foldLeft[A,B](list: LinkedList[A], acc: B)(f: (B, A) => B): B = list match {
+    case Nil => acc
+    case Cons(x, xs) => foldLeft(xs, f(acc, x))(f)
+  }
+
+  def foldRightUnsafe[A, B](list: LinkedList[A], acc: B)(f: (A, B) => B): B = list match {
+    case Nil => acc
+    case Cons(x, xs) => f(x, foldRight(xs, acc)(f))
+  }
+
+  def foldRight[A, B](list: LinkedList[A], acc: B)(f: (A, B) => B): B =
+    foldLeft(reverse(list), acc)((b,a) => f(a,b))
+
+  def reverse[A](list: LinkedList[A]): LinkedList[A] =
+    foldLeft(list, Nil:LinkedList[A])((xs: LinkedList[A], x: A) => Cons(x, xs))
+
+  def len[A](list: LinkedList[A]): Long =
+    foldLeft(list, 0)((acc, _) => acc + 1)
+
+  def sum[N](nums: LinkedList[N])(implicit numeric: Numeric[N]): N =
+    foldLeft(nums, numeric.zero)(numeric.plus)
+
+  def product[N](nums: LinkedList[N])(implicit numeric: Numeric[N]): N =
+    foldLeft(nums, numeric.one)(numeric.times)
+
+  def sumCase[N](nums: LinkedList[N])(implicit numeric: Numeric[N]): N = nums match {
     case Nil => numeric.zero
     case Cons(x, xs) => numeric.plus(x, sum(xs))
   }
 
-  def product[N](nums: LinkedList[N])(implicit numeric: Numeric[N]): N = nums match {
+  def productCase[N](nums: LinkedList[N])(implicit numeric: Numeric[N]): N = nums match {
     case Nil => numeric.one
-    case Cons(0, _) => numeric.zero
     case Cons(x, xs) => numeric.times(x, product(xs))
   }
 
@@ -22,10 +49,8 @@ object LinkedList {
     list match {
       case Nil => Nil
       case Cons(x, xs) => {
-        if (f.apply(x))
-          dropWhile(xs, f)
-        else
-          Cons(x, xs)
+        if (f.apply(x)) dropWhile(xs, f)
+        else Cons(x, xs)
       }
     }
 
@@ -40,8 +65,38 @@ object LinkedList {
 
   def tail[A](list: LinkedList[A]): LinkedList[A] = drop(list, 1)
 
-  def setHead[A](a: A, list: LinkedList[A]) = list match {
+  def setHead[A](a: A, list: LinkedList[A]): LinkedList[A] = list match {
     case Nil => Nil
     case Cons(_, x) => Cons(a, x)
   }
+
+  def init[A](list: LinkedList[A]): LinkedList[A] = list match {
+    case Nil => Nil
+    case Cons(_, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+  }
+
+  def append[A](list1: LinkedList[A], list2: LinkedList[A]): LinkedList[A] =
+    foldRight(list1, list2)(Cons(_, _))
+
+  def appendCase[A](list1: LinkedList[A], list2: LinkedList[A]): LinkedList[A] =
+    list1 match {
+      case Nil => list2
+      case Cons(x, xs) => Cons(x, appendCase(xs, list2))
+    }
+
+  def flatten[A](list: LinkedList[LinkedList[A]]): LinkedList[A] =
+    foldRight(list, Nil:LinkedList[A])(append)
+
+  def map[A,B](list: LinkedList[A])(f: A => B): LinkedList[B] =
+    foldRight(list, Nil:LinkedList[B])((a, xs) => Cons(f(a), xs))
+
+  def flatMap[A,B](list: LinkedList[A])(f: A => LinkedList[B]): LinkedList[B] =
+    foldRight(list, Nil:LinkedList[B])((a, xs) => append(f(a), xs))
+
+  def where[A](list: LinkedList[A])(f: A => Boolean): LinkedList[A] =
+    foldRight(list, Nil:LinkedList[A])((a, xs) => f(a) match {
+      case true => Cons(a, xs)
+      case false => xs
+  })
 }
--- src/test/scala/Ch3LinkedListSpec.scala
+++ src/test/scala/Ch3LinkedListSpec.scala
@@ -1,15 +1,29 @@
 import LinkedList._
+import org.scalacheck.Gen
+import org.scalacheck.Gen.listOf
 import org.specs2.ScalaCheck
 import org.specs2.matcher.DataTables
 import org.specs2.mutable.Specification
 
+import scala.util.Random
+
 class Ch3LinkedListSpec extends Specification with ScalaCheck with DataTables {
+  val listOfTrues = listOf(Gen.const(true))
+  val listOfFalses = listOf(Gen.const(false))
 
   "#apply" >> {
     "shorthand constructor" >> {
       LinkedList(1,2,3) mustEqual Cons(1, Cons(2, Cons(3, Nil)))
       LinkedList() mustEqual Nil
       LinkedList(Seq(): _*) mustEqual Nil
+    }
+  }
+
+  "#length" >> {
+    "seq length equivalent to linkedlist length" >> {
+      prop {
+        seq: Seq[Int] =>  seq.length mustEqual len(LinkedList(seq: _*))
+      }
     }
   }
 
@@ -58,7 +72,8 @@ class Ch3LinkedListSpec extends Specification with ScalaCheck with DataTables {
   "#tail" >> {
     "removes the head if possible" >> {
       prop {
-        (a: Int, b: Seq[Int]) => tail(Cons(a, LinkedList(b))) mustEqual LinkedList(b)
+        (a: Int, b: Seq[Int]) => tail(Cons(a, LinkedList(b: _*))) mustEqual
+          LinkedList(b: _*)
       }
     }
   }
@@ -66,9 +81,81 @@ class Ch3LinkedListSpec extends Specification with ScalaCheck with DataTables {
   "#setHead" >> {
     "return new list replacing the head" >> {
       prop {
-        (a: Int, b: Int, s: Seq[Int]) => setHead(a, Cons(b, LinkedList(s))) mustEqual
-          Cons(a, LinkedList(s))
+        (a: Int, b: Int, s: Seq[Int]) => setHead(a, Cons(b, LinkedList(s: _*))) mustEqual
+          Cons(a, LinkedList(s: _*))
       }
+    }
+  }
+
+  "#init" >> {
+    "un-does append of a single item" >> {
+      prop {
+        (a: Int, xs: Seq[Int]) => init(append(LinkedList(xs: _*), Cons(a, Nil))) mustEqual
+          LinkedList(xs: _*)
+      }
+    }
+  }
+
+  "#reverse" >> {
+    "composition is identity" >> {
+      prop {
+        xs: Seq[Int] => reverse(reverse(LinkedList(xs: _*))) mustEqual
+          LinkedList(xs: _*)
+      }
+    }
+
+    "empty or single item lists unchanged" >> {
+      reverse(Nil) mustEqual Nil
+      reverse(Cons(1, Nil)) mustEqual Cons(1, Nil)
+    }
+
+    "three element lists are reversed" >> {
+      prop {
+        (a: Int, b: Int, c: Int) => reverse(Cons(a, Cons(b, Cons(c, Nil)))) mustEqual
+          Cons(c, Cons(b, Cons(a, Nil)))
+      }
+    }
+  }
+
+  "#flatten" >> {
+    "example" >> {
+      flatten(LinkedList(LinkedList(1,2,3), Nil, LinkedList(1))) mustEqual
+        LinkedList(1,2,3,1)
+    }
+
+    "Seq[Seq[a]] => Seq[a]" >> {
+      prop {
+        (a: Seq[Int], b: Seq[Int]) => flatten(
+          LinkedList(LinkedList(a: _*), LinkedList(b: _*))) mustEqual
+          append(LinkedList(a: _*), LinkedList(b: _*))
+      }
+    }
+  }
+
+//  "#append" >> {
+//
+//  }
+
+  "#map" >> {
+    "identity leaves list unchanged" >> {
+      prop {
+        a: Seq[Int] => LinkedList.map(LinkedList(a: _*))(i => i) mustEqual
+          LinkedList(a: _*)
+      }
+    }
+  }
+
+//  "flatMap" >> {
+//
+//  }
+
+  "#where" >> {
+    "only true values remain" >> {
+      prop((trues: List[Boolean], falses: List[Boolean]) => {
+        val all = LinkedList(Random.shuffle(trues ++ falses): _*)
+
+        LinkedList(trues: _*) mustEqual where(all)(_ == true)
+      }).setGens(listOfTrues, listOfFalses)
     }
   }
 }
